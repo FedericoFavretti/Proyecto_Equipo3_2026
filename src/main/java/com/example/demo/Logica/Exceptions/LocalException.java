0be13881a87248ee3ab0951416a492d46b6ad36b
Facu185 <facufa12@gmail.com>
2026-06-21 16:57:28 -0300

    .

---
+++ src/main/java/com/example/demo/Logica/Exceptions/LocalException.java
@@ -0,0 +1,8 @@
+package com.example.demo.Logica.Exceptions;
+
+public class LocalException extends RuntimeException {
+    public LocalException(String message) {
+        super(message);
+    }
+
+}
