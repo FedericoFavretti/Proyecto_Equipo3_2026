12a285e2d429356a9113d50b10a7b8def9c7d462
Facu185 <facufa12@gmail.com>
2026-06-26 19:28:36 -0300

    .

--- src/main/java/com/example/demo/Logica/Interfaces/iPedidoController.java
+++ src/main/java/com/example/demo/Logica/Interfaces/iPedidoController.java
@@ -1,22 +1,16 @@
 package com.example.demo.Logica.Interfaces;
 
-import com.example.demo.Logica.Clases.Pedido;
 import com.example.demo.Logica.DataTypes.request.DtConfirmarPedidoRequest;
 import com.example.demo.Logica.DataTypes.request.DtPedidoListadoFiltro;
 import com.example.demo.Logica.DataTypes.request.DtRechazarPedidoRequest;
-import com.example.demo.Logica.DataTypes.shared.DtPedido;
 import com.example.demo.Logica.DataTypes.shared.DtPedidoConDetalles;
 import com.example.demo.Logica.DataTypes.summary.DtPedidoListadoResponse;
 import com.example.demo.Logica.DataTypes.response.DtPedidoResponse;
-import com.example.demo.Logica.Enums.EstadoPedido;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface iPedidoController {
