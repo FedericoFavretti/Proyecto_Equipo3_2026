80dae0bd6aa2a08575b7583a7185c7941148e146
Facu185 <facufa12@gmail.com>
2026-06-26 19:35:14 -0300

    .

--- src/main/java/com/example/demo/Logica/Interfaces/iLocalController.java
+++ src/main/java/com/example/demo/Logica/Interfaces/iLocalController.java
@@ -8,11 +8,9 @@ import com.example.demo.Logica.Clases.Promocion;
 import com.example.demo.Logica.DataTypes.request.DtPromocionRequest;
 import com.example.demo.Logica.DataTypes.request.DtFiltroClienteLocal;
 import com.example.demo.Logica.DataTypes.response.DtClienteLocalResponse;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
