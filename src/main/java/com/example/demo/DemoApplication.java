999db5604017b29fb5d1bf2d691c8e933b997834
joaquinortiz337 <joaquinortiz636@gmail.com>
2026-07-04 23:05:08 -0300

    Zona horaria

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Montevideo"));
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
