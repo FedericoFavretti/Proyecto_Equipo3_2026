b17689cfd791d79bb1098ca45762f6b34361d4ab
Facu185 <facufa12@gmail.com>
2026-07-05 21:58:02 -0300

    .

--- src/main/java/com/example/demo/config/MercadoPagoConfiguration.java
+++ src/main/java/com/example/demo/config/MercadoPagoConfiguration.java
@@ -1,10 +1,10 @@
 package com.example.demo.config;
 
 import com.mercadopago.MercadoPagoConfig;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 
 @Configuration
 public class MercadoPagoConfiguration {
