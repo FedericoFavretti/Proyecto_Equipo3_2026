66dcb2eaa260a6da06190443f43ae7b11432fa40
joaquinortiz337 <joaquinortiz636@gmail.com>
2026-07-09 23:48:14 -0300

    Paginacion

--- src/main/java/com/example/demo/Logica/Interfaces/iClienteController.java
+++ src/main/java/com/example/demo/Logica/Interfaces/iClienteController.java
@@ -1,25 +1,17 @@
 package com.example.demo.Logica.Interfaces;
 
 import com.example.demo.Logica.Clases.Cliente;
-import com.example.demo.Logica.DataTypes.request.DtFiltro;
-import com.example.demo.Logica.DataTypes.request.DtFiltroLocal;
 import com.example.demo.Logica.DataTypes.request.DtGoogleAuthRequest;
 import com.example.demo.Logica.DataTypes.request.DtGoogleRegistroCompletarRequest;
 import com.example.demo.Logica.DataTypes.response.DtBusquedaPlatosPromocionesResponse;
 import com.example.demo.Logica.DataTypes.response.DtGoogleRegistroPendienteResponse;
 import com.example.demo.Logica.DataTypes.response.DtLocalBusquedaResponse;
 import com.example.demo.Logica.DataTypes.response.DtLoginResponseCliente;
+import com.example.demo.Logica.DataTypes.response.DtPagina;
 import com.example.demo.Logica.DataTypes.shared.DtCliente;
 import org.springframework.http.ResponseEntity;
-import com.example.demo.Logica.DataTypes.request.DtFiltro;
-import com.example.demo.Logica.DataTypes.request.DtFiltroLocal;
-import com.example.demo.Logica.DataTypes.response.DtPagina;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 public interface iClienteController {
     ResponseEntity<Cliente> registrarUsuario(@RequestPart("datos") DtCliente dtCliente, @RequestPart("foto") MultipartFile foto);
