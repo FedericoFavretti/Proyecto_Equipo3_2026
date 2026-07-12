30a982920d9eea11b37e5d7719ee583d242223a5
Roibeth Garcia <roibethgarcia9@gmail.com>
2026-07-11 23:08:04 -0300

    fix

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/v1/usuarios/login",
                                 "/api/v1/usuarios/activar",
+                                "/api/v1/usuarios/reenviar-activacion",
                                 "/api/v1/usuarios/recuperar_contra_correo",
                                 "/api/v1/usuarios/recuperar"
                         ).permitAll()
