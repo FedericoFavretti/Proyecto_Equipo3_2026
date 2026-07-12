ffbb0253e34fc4ec5241a357bb3756acce6a52d9
Facu185 <facufa12@gmail.com>
2026-07-11 23:08:19 -0300

    .

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -30,8 +30,7 @@ public class SecurityConfig {
                                 "/api/v1/usuarios/login",
                                 "/api/v1/usuarios/activar",
                                 "/api/v1/usuarios/recuperar_contra_correo",
-                                "/api/v1/usuarios/recuperar",
-                                "/api/v1/usuarios//reenviar-activacion"
+                                "/api/v1/usuarios/recuperar"
                         ).permitAll()
                         .requestMatchers(
                                 "/api/v1/clientes/registro",
